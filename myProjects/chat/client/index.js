3571ca46291b6d0593969e7adae85dd70790dce8
Luan Lopes <luan.lopes@clickativo.com.br>
2023-07-04 23:19:10 -0300

    feature: continuing with chat app implementation

--- myProjects/chat/client/index.js
+++ myProjects/chat/client/index.js
@@ -3,6 +3,7 @@ const socket = new WebSocket("ws://localhost:5001");
 const personalizedEvent = { event: "test" };
 
 socket.addEventListener("open", (event) => {
+  console.log("socket connected");
   socket.send(JSON.stringify(personalizedEvent));
 });
 
--- myProjects/chat/server/index.js
+++ myProjects/chat/server/index.js
@@ -1,4 +1,5 @@
 import { createServer } from "node:net";
+import { createHash } from "node:crypto";
 
 // Chat:
 // The goal of this project is to:
@@ -6,29 +7,63 @@ import { createServer } from "node:net";
 // Understand how Node Net module works and how to work with sockets;
 // Further learning in streams;
 
+const MAGIC_STRING = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+
+function hash(data) {
+  const hash = createHash("sha1");
+
+  hash.update(data);
+  return hash.digest("base64");
+}
+
+function getHeaders(data) {
+  const headers = {};
+
+  const lines = data.split("\n");
+
+  if (lines[0].includes("GET") && lines[0].includes("HTTP/1.1")) {
+    lines
+      .filter((head) => head.includes(":"))
+      .forEach((head) => {
+        const [key, value] = head.split(":");
+        headers[key] = value.replaceAll("\r", "").trim();
+      });
+
+    return headers;
+  }
+
+  return null;
+}
+
+function createHandShakeHeaders(headers) {
+  const isUpgradingToWs = headers["Upgrade"] === "websocket";
+  const isUpgradingConnection = headers["Connection"].includes("Upgrade");
+  if (isUpgradingConnection && isUpgradingToWs) {
+    const secWebSocketKey = headers["Sec-WebSocket-Key"];
+    const secWebSocketAccept = hash(secWebSocketKey + MAGIC_STRING);
+
+    return [
+      "HTTP/1.1 101 Switching Protocols\r\n",
+      "Upgrade: websocket\r\n",
+      "Connection: Upgrade\r\n",
+      `Sec-WebSocket-Accept: ${secWebSocketAccept}\r\n`,
+      "\r\n",
+    ];
+  }
+
+  return [];
+}
+
 const server = createServer((connection) => {
-  //console.log("connected: ", connection); // connection information;
-  //const testHeader = [
-  //  "HTTP/1.1 101 Switching Protocols",
-  //  "Upgrade: websocket",
-  //  "Connection: Upgrade",
-  //];
-  //connection.write(testHeader.map((line) => line.concat("\r\n")).join(""));
-  //connection.pipe(connection);
-  //connection.on("data", (data) => {
-  //  //const headers = data.toString().replace(/\r/g, "").split("\n");
-  //  console.log(data.toString());
-  //});
-  //connection.pipe(connection); //writing back to the connection;
   connection.on("data", (data) => {
-    const [header, body] = data.toString().split(/^\n$/);
-    console.log(header);
-    console.log(body);
+    const headers = getHeaders(data.toString());
+    if (headers) {
+      const handShakeHeaders = createHandShakeHeaders(headers);
+      connection.write(handShakeHeaders.join(""));
+      return;
+    }
 
-    const enhanceBody = JSON.parse(body);
-    enhanceBody.result = "success";
-
-    connection.write(JSON.stringify(enhanceBody));
+    console.log(data.readUInt8());
   });
 });
 
